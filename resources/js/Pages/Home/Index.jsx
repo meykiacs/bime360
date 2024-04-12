34b71cd20d04e862c0bb5efbf71b123216357df7
meykiacs <meykiacs@gmail.com>
2024-04-12 14:49:30 +0330

    added curve for the homepage in mobile view

--- resources/js/Pages/Home/Index.jsx
+++ resources/js/Pages/Home/Index.jsx
@@ -1,6 +1,7 @@
 import Container from "@/Components/Container"
 import FullBleedAbsolute from "@/Components/FullBleedAbsolute"
 import MobileHeader from "@/Components/MobileHeader"
+import HomepageCurve from "@/Components/homepageCurve"
 import React from "react"
 
 function Index() {
@@ -22,11 +23,7 @@ function Index() {
                 <h2 className="relative top-[67px] z-30 text-center text-xl font-bold text-white">
                     از صدور تا خسارت 360 درجه با شما هستیم
                 </h2>
-                {/* <div className="absolute left-1/2 top-1/4 z-40 h-[205px] w-[410px] -translate-x-1/2 transform rounded-bl-[205px] rounded-br-[205px] border-b-4 border-t-0 border-primary-400 bg-white"></div> */}
-                <div className="absolute left-1/2 top-[55%] z-40 h-[200px] w-[200px] -translate-x-[100%] transform rounded-bl-[200px] border-4 border-white opacity-50 border-t-0 border-r-0 bg-transparent">
-                    <div className="bg-white w-2 h-2 transform -translate-x-[200px] z-50 opacity-1"></div>
-                </div>
-                <div className="absolute right-1/2 top-[55%] z-30 h-[200px] w-[200px] translate-x-[100%] transform rounded-br-[200px] border-4 border-primary-400 border-t-0 border-l-0"></div>
+                <HomepageCurve className="absolute z-40 left-[50%] transform -translate-x-[50%] top-[57%]" width="400" height="400" />
             </Container>
         </>
     )
